67cfeb06e32f2ee1e214dc862ce5b2458f867e8d
ZhongJinHacker <jiangzhongjin@hotmail.com>
2020-01-02 17:59:39 +0800

    搭架子2

---
+++ src/main/java/com/grady/fim/common/utils/ResultTool.java
@@ -0,0 +1,26 @@
+package com.grady.fim.common.utils;
+
+import com.grady.fim.common.pojo.bo.JsonResult;
+import com.grady.fim.common.pojo.enums.ResultCode;
+
+/**
+ * @author gradyjiang
+ * @Date 2020/1/2 - 下午5:56
+ */
+public class ResultTool {
+    public static JsonResult success() {
+        return new JsonResult(true);
+    }
+
+    public static <T> JsonResult<T> success(T data) {
+        return new JsonResult<T>(true, data);
+    }
+
+    public static JsonResult fail() {
+        return new JsonResult(false);
+    }
+
+    public static JsonResult fail(ResultCode resultEnum) {
+        return new JsonResult(false, resultEnum);
+    }
+}
