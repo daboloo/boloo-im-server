6c621580cf93ea9b267befc4c678ebecb08cd517
ZhongJinHacker <jiangzhongjin@hotmail.com>
2020-01-02 16:36:59 +0800

    init

---
+++ src/main/java/com/grady/fim/FimApplication.java
@@ -0,0 +1,16 @@
+package com.grady.fim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zhongjinHacker
+ */
+@SpringBootApplication
+public class FimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FimApplication.class, args);
+	}
+
+}
