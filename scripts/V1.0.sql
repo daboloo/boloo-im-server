df7ff58a8341c1594fed2a6083ba5fbf3aa27de9
ZhongJinHacker <jiangzhongjin@hotmail.com>
2020-01-03 11:39:43 +0800

    权限控制sql脚本

---
+++ scripts/V1.0.sql
@@ -0,0 +1,1 @@
+CREATE DATABASE fim_db;
---
+++ scripts/V2.0.sql
@@ -0,0 +1,45 @@
+USE fim_db;
+
+# 用户表
+DROP TABLE IF EXISTS `fim_user`;
+CREATE TABLE `fim_user` (
+`id` BIGINT(11) NOT NULL AUTO_INCREMENT,
+`username` VARCHAR(255) NOT NULL COMMENT '用户名',
+`password` VARCHAR(255) NOT NULL COMMENT '密码',
+PRIMARY KEY (`id`)
+) ENGINE=INNODB DEFAULT CHARSET=utf8mb4 COMMENT='用户表';
+
+# 角色表
+DROP TABLE IF EXISTS `fim_role`;
+CREATE TABLE `fim_role` (
+`id` BIGINT(11) NOT NULL AUTO_INCREMENT,
+`name` VARCHAR(255) NOT NULL COMMENT '角色名',
+PRIMARY KEY (`id`)
+)ENGINE=INNODB DEFAULT CHARSET=utf8mb4 COMMENT='角色表';
+
+# 用户角色关系表
+DROP TABLE IF EXISTS `fim_user_role`;
+CREATE TABLE `fim_user_role` (
+`id` BIGINT(11) NOT NULL AUTO_INCREMENT,
+`user_id` BIGINT(11) NOT NULL COMMENT '用户id',
+`role_id` BIGINT(11) NOT NULL COMMENT '角色id',
+PRIMARY KEY (`id`),
+UNIQUE KEY  `idx_user_role` (`user_id`, `role_id`)
+) ENGINE=INNODB DEFAULT CHARSET=utf8mb4 COMMENT='用户与角色关系表';
+
+# 权限表
+DROP TABLE IF EXISTS `fim_permission`;
+CREATE TABLE `fim_permission` (
+`id` BIGINT(11) NOT NULL AUTO_INCREMENT,
+`url` VARCHAR(255) NOT NULL COMMENT '权限允许访问路径',
+`name` VARCHAR(255) NOT NULL COMMENT '权限名',
+`description` VARCHAR(255) NULL COMMENT '描述',
+PRIMARY KEY (`id`)
+)ENGINE=INNODB DEFAULT CHARSET=utf8mb4 COMMENT='权限表';
+
+# 角色与权限关系表
+DROP TABLE IF EXISTS `fim_role_permission`;
+CREATE TABLE `fim_role_permission` (
+`role_id` BIGINT(11) NOT NULL COMMENT '角色id',
+`permission_id` BIGINT(11) NOT NULL COMMENT '权限id'
+)ENGINE=INNODB DEFAULT CHARSET=utf8mb4 COMMENT='角色与权限关系表';
