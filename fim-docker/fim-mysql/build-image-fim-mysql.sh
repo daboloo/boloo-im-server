e199af562cda7214526fe0144837e1e3d07e89dd
ZhongJinHacker <jiangzhongjin@hotmail.com>
2020-02-03 20:31:11 +0800

    fix

--- fim-docker/fim-mysql/build-image-fim-mysql.sh
+++
@@ -1,3 +0,0 @@
-echo "开始构建 fim-mysql 镜像..."
-
-docker build -t fim-mysql:1.0 .
--- fim-docker/fim-mysql/install_data.sh
+++ fim-docker/fim-mysql/install_data.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 mysql -uroot -p$MYSQL_ROOT_PASSWORD <<EOF
 source $WORK_PATH/$DDL_SQL;
-source $WORK_PATH/DML_SQL;
+source $WORK_PATH/$DML_SQL;
